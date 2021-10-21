ea796baf40c53559734d07c63187203ef0eb5614
Zach Hazelton <hazeltzk@mail.uc.edu>
2021-10-20 17:16:42 -0700

    Swift Project file commit

---
+++ ShareCat/ShareCat/ShareCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareCatApp.swift
+//  ShareCat
+//
+//  Created by Zachary Hazelton on 10/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ShareCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
