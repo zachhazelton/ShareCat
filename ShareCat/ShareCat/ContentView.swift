55f5dafcc719b2e2a7aec90d44c7bb098512d230
Zach Hazelton <hazeltzk@mail.uc.edu>
2021-11-01 12:13:27 -0700

    Added pages

--- ShareCat/ShareCat/ContentView.swift
+++ ShareCat/ShareCat/ContentView.swift
@@ -9,18 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-    
-    /*VStack(alignment: .leading){
-        Button("Home"){
-        
-        }
-    
-    }
-     */
-}
+        NavigationView {
+            NavigationLink(destination: Text("Calendar Page")) {
+            Text("Calendar")
+                .padding()
+                .navigationBarTitle("Home", displayMode: .large)
+                VStack(alignment: .leading){
+                    NavigationLink(destination: Text("Map Page")) {
+                        Text("Map")
+                    }
+                    NavigationLink(destination: Text("Campus Resources Page")) {
+                        Text("Campus Resources")
+                    }
+                    NavigationLink(destination: Text("Clubs Page")) {
+                        Text("Clubs")
+                    }
+                }
+            }
+        }//end nav view
+    }//end body
+
+}//end view
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -28,8 +38,10 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+//Home button code for main menu
 /*struct Main_Menu: View {
     var home_button:
 
 }
  */
+
