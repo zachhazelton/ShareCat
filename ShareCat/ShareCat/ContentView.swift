6f6b7aae3a8fe0f258d75cbf02a51c854d9539db
Rozierjn <91701606+Rozierjn@users.noreply.github.com>
2021-11-13 01:38:14 -0800

    Added in Calendar View

--- ShareCat/ShareCat/ContentView.swift
+++ ShareCat/ShareCat/ContentView.swift
@@ -89,7 +89,46 @@ struct ClubsView: View{
 
 struct CalendarView: View{
     var body: some View{
-        Text("This is the calendar view")
+        import UIKit
+import EventKit
+
+struct EventLocation {
+    let title: String
+    let latitude: Double
+    let longitude: Double
+}
+
+public struct CalendarEvent {
+    public let title: String
+    public let startDate: Date
+    public let endDate:Date
+    
+    public init(title: String, startDate: Date, endDate: Date) {
+        self.title = title;
+        self.startDate = startDate;
+        self.endDate = endDate;
+    }
+}
+
+public protocol CalendarViewDataSource {
+    func startDate() -> Date
+    func endDate() -> Date
+    /* optional */
+    func headerString(_ date: Date) -> String?
+}
+
+extension CalendarViewDataSource {
+    
+    func startDate() -> Date {
+        return Date()
+    }
+    func endDate() -> Date {
+        return Date()
+    }
+    
+    func headerString(_ date: Date) -> String? {
+        return nil
+    }
     }
 }
 
