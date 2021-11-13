dda16b81cc07bbd36aad0811bc72e112ba49fc70
Rozierjn <91701606+Rozierjn@users.noreply.github.com>
2021-11-13 01:32:45 -0800

    Updated struct MapView to add in MapView Code
    Added in Map View

--- ShareCat/ShareCat/ContentView.swift
+++ ShareCat/ShareCat/ContentView.swift
@@ -102,7 +102,25 @@ struct ResourcesView: View{
 
 struct MapView: View{
     var body: some View{
-        Text("This is the map view")
+        import SwiftUI
+import MapKit
+
+struct ContentView: View {
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 39.1329, longitude: 84.51),
+        span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+
+    var body: some View {
+        Map(coordinateRegion:$region)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
     }
 }
 
