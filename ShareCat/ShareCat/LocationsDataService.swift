3eceae77d24798cfa9aee374accfa9fa7823fc52
Rozierjn <91701606+Rozierjn@users.noreply.github.com>
2022-02-09 13:23:40 -0500

    Update LocationsDataService.swift

--- ShareCat/ShareCat/LocationsDataService.swift
+++ ShareCat/ShareCat/LocationsDataService.swift
@@ -12,9 +12,9 @@ class LocationsDataService {
     
     static let locations: [Location] = [
         Location(
-            name: "Baldwin",
+            name: "Baldwin Hall",
             cityName: "Cincinnati",
-            coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922),
+            coordinates: CLLocationCoordinate2D(latitude: 39.1328, longitude: -84.5166),
             description: "The Colosseum is an oval amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum. It is the largest ancient amphitheatre ever built, and is still the largest standing amphitheatre in the world today, despite its age.",
             imageNames: [
                 "rome-colosseum-1",
@@ -25,7 +25,7 @@ class LocationsDataService {
         Location(
             name: "Tangeman University Center",
             cityName: "Cincinnati",
-            coordinates: CLLocationCoordinate2D(latitude: 41.8986, longitude: 12.4769),
+            coordinates: CLLocationCoordinate2D(latitude: 39.1320, longitude: -84.5172),
             description: "The Pantheon is a former Roman temple and since the year 609 a Catholic church, in Rome, Italy, on the site of an earlier temple commissioned by Marcus Agrippa during the reign of Augustus.",
             imageNames: [
                 "rome-pantheon-1",
@@ -36,7 +36,7 @@ class LocationsDataService {
         Location(
             name: "Campus Recreation Center",
             cityName: "Cincinnati",
-            coordinates: CLLocationCoordinate2D(latitude: 41.9009, longitude: 12.4833),
+            coordinates: CLLocationCoordinate2D(latitude: 39.1324, longitude: -84.5154),
             description: "The Trevi Fountain is a fountain in the Trevi district in Rome, Italy, designed by Italian architect Nicola Salvi and completed by Giuseppe Pannini and several others. Standing 26.3 metres high and 49.15 metres wide, it is the largest Baroque fountain in the city and one of the most famous fountains in the world.",
             imageNames: [
                 "rome-trevifountain-1",
@@ -47,7 +47,7 @@ class LocationsDataService {
         Location(
             name: "French Hall",
             cityName: "Cincinnati",
-            coordinates: CLLocationCoordinate2D(latitude: 48.8584, longitude: 2.2945),
+            coordinates: CLLocationCoordinate2D(latitude: 39.1323, longitude: -84.5126),
             description: "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower. Locally nicknamed 'La dame de fer', it was constructed from 1887 to 1889 as the centerpiece of the 1889 World's Fair and was initially criticized by some of France's leading artists and intellectuals for its design, but it has become a global cultural icon of France and one of the most recognizable structures in the world.",
             imageNames: [
                 "paris-eiffeltower-1",
@@ -57,7 +57,7 @@ class LocationsDataService {
         Location(
             name: "Nippert Stadium",
             cityName: "Cincinnati",
-            coordinates: CLLocationCoordinate2D(latitude: 48.8606, longitude: 2.3376),
+            coordinates: CLLocationCoordinate2D(latitude: 39.1314, longitude: -84.5170),
             description: "The Louvre, or the Louvre Museum, is the world's most-visited museum and a historic monument in Paris, France. It is the home of some of the best-known works of art, including the Mona Lisa and the Venus de Milo. A central landmark of the city, it is located on the Right Bank of the Seine in the city's 1st arrondissement.",
             imageNames: [
                 "paris-louvre-1",
