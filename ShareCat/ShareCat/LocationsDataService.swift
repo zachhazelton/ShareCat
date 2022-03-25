6667c27477a4ddb7b97f97758609beb2118d80a6
Rozierjn <91701606+Rozierjn@users.noreply.github.com>
2022-03-24 22:30:11 -0400

    Update LocationsDataService.swift

--- ShareCat/ShareCat/LocationsDataService.swift
+++ ShareCat/ShareCat/LocationsDataService.swift
@@ -39,9 +39,9 @@ class LocationsDataService {
             coordinates: CLLocationCoordinate2D(latitude: 39.1324, longitude: -84.5154),
             description: "Campus Recreation Center Hall is perfect for students that desire productivity and focus in their day-to-day life.  It is located on MainStreet connected to the Rec Center and CenterCourt dining hall, making it the perfect environment for motivation. Being so close to the Rec inspires students to take advantage of its health benefits and helps them get involved in the countless events, activities and programs that our Rec Center has to offer.",
             imageNames: [
-                "CampusRecreationCenter",
-                "rome-trevifountain-2",
-                "rome-trevifountain-3",
+                "CampusRecreationCenter1",
+                "CampusRecreationCenter2",
+                "CampusRecreationCenter3",
             ],
             link: "https://en.wikipedia.org/wiki/Trevi_Fountain"),
         Location(
@@ -50,8 +50,8 @@ class LocationsDataService {
             coordinates: CLLocationCoordinate2D(latitude: 39.1323, longitude: -84.5126),
             description: "In the center of East Campus is French Hall. This building is where you want to go for advising and testing.",
             imageNames: [
-                "paris-eiffeltower-1",
-                "paris-eiffeltower-2",
+                "FrenchHall1",
+                "FrenchHall2",
             ],
             link: "https://en.wikipedia.org/wiki/Eiffel_Tower"),
         Location(
@@ -60,9 +60,9 @@ class LocationsDataService {
             coordinates: CLLocationCoordinate2D(latitude: 39.1314, longitude: -84.5170),
             description: "Nippert Stadium is an outdoor stadium in Cincinnati, Ohio, on the campus of the University of Cincinnati. Primarily used for American football, it is the home field of the Cincinnati Bearcats football team.",
             imageNames: [
-                "paris-louvre-1",
-                "paris-louvre-2",
-                "paris-louvre-3",
+                "NippertStadium1",
+                "NippertStadium2",
+                "NippertStadium3",
             ],
             link: "https://en.wikipedia.org/wiki/Louvre"),
     ]
