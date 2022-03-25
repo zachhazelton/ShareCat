394e41256a2230437790f80e30c8eb46f84045a6
Rozierjn <91701606+Rozierjn@users.noreply.github.com>
2022-03-24 22:05:52 -0400

    Update LocationsDataService.swift

--- ShareCat/ShareCat/LocationsDataService.swift
+++ ShareCat/ShareCat/LocationsDataService.swift
@@ -17,9 +17,9 @@ class LocationsDataService {
             coordinates: CLLocationCoordinate2D(latitude: 39.1328, longitude: -84.5166),
             description: "Baldwin Hall is situated at the heart of Schneider Quad, formerly known as Baldwin Hall, adorned with beautiful white columns at its entrance.  It was the first building constructed for the Engineering Department at the University of Cincinnati.  Named for benefactor, Frances H. Baldwin, it was built in 1911 for the sole purpose of being utilized by the engineering department.",
             imageNames: [
-                "rome-colosseum-1",
-                "rome-colosseum-2",
-                "rome-colosseum-3",
+                "BaldwinHall1",
+                "BaldwinHall2",
+                "BaldwinHall3",
             ],
             link: "https://en.wikipedia.org/wiki/Colosseum"),
         Location(
@@ -28,9 +28,9 @@ class LocationsDataService {
             coordinates: CLLocationCoordinate2D(latitude: 39.1320, longitude: -84.5172),
             description: "In the center of West Campus is Tangeman University Center.  Architects Harry Hake and Harry Hake Jr. built TUC in 1937 in response to a growing student population. The building was constructed with a Georgian Colonial style and highlighted by the large columns at the entrance and the clock tower.",
             imageNames: [
-                "rome-pantheon-1",
-                "rome-pantheon-2",
-                "rome-pantheon-3",
+                "Tangeman1",
+                "Tangeman2",
+                "Tangeman3",
             ],
             link: "https://en.wikipedia.org/wiki/Pantheon,_Rome"),
         Location(
@@ -39,7 +39,7 @@ class LocationsDataService {
             coordinates: CLLocationCoordinate2D(latitude: 39.1324, longitude: -84.5154),
             description: "Campus Recreation Center Hall is perfect for students that desire productivity and focus in their day-to-day life.  It is located on MainStreet connected to the Rec Center and CenterCourt dining hall, making it the perfect environment for motivation. Being so close to the Rec inspires students to take advantage of its health benefits and helps them get involved in the countless events, activities and programs that our Rec Center has to offer.",
             imageNames: [
-                "rome-trevifountain-1",
+                "CampusRecreationCenter",
                 "rome-trevifountain-2",
                 "rome-trevifountain-3",
             ],
@@ -48,7 +48,7 @@ class LocationsDataService {
             name: "French Hall",
             cityName: "Cincinnati",
             coordinates: CLLocationCoordinate2D(latitude: 39.1323, longitude: -84.5126),
-            description: "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower. Locally nicknamed 'La dame de fer', it was constructed from 1887 to 1889 as the centerpiece of the 1889 World's Fair and was initially criticized by some of France's leading artists and intellectuals for its design, but it has become a global cultural icon of France and one of the most recognizable structures in the world.",
+            description: "In the center of East Campus is French Hall. This building is where you want to go for advising and testing.",
             imageNames: [
                 "paris-eiffeltower-1",
                 "paris-eiffeltower-2",
@@ -58,7 +58,7 @@ class LocationsDataService {
             name: "Nippert Stadium",
             cityName: "Cincinnati",
             coordinates: CLLocationCoordinate2D(latitude: 39.1314, longitude: -84.5170),
-            description: "The Louvre, or the Louvre Museum, is the world's most-visited museum and a historic monument in Paris, France. It is the home of some of the best-known works of art, including the Mona Lisa and the Venus de Milo. A central landmark of the city, it is located on the Right Bank of the Seine in the city's 1st arrondissement.",
+            description: "Nippert Stadium is an outdoor stadium in Cincinnati, Ohio, on the campus of the University of Cincinnati. Primarily used for American football, it is the home field of the Cincinnati Bearcats football team.",
             imageNames: [
                 "paris-louvre-1",
                 "paris-louvre-2",
